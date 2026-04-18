0afeb0498267e1c7c8d71ee5d36d65b1fe960b31
kishblockpro <narkishen@gmail.com>
2026-04-18 16:11:14 +0530

    Update q1.s
    converted from riscv 32 - 64 
    
    lw sw -> ld, sd
    pointer size from 4 -> 8
    malloc 12 -> 24 
    insert stack alloc 16 -> 32

--- q1/q1.s
+++ q1/q1.s
@@ -3,116 +3,95 @@
 .globl get
 .globl getAtMost
 
-make_node: 
-  addi sp, sp, -16 #allocate memory for ra and s0
-  sw ra, 12(sp)
-  sw s0, 8(sp)
 
-  addi s0, a0, 0 #store val in s0  
 
-  li a0, 12
-  jal ra, malloc #a0 contains the pointer malloc returns
-  
+make_node:
+  addi sp, sp, -16
+  sd ra, 8(sp)
+  sd s0, 0(sp)
+  addi s0, a0, 0
+  li a0, 24
+  jal ra, malloc
+
   sw s0, 0(a0)
-  sw zero, 4(a0)
-  sw zero, 8(a0)
-
-  lw s0, 8(sp)  #restore ra and s0
-  lw ra, 12(sp)
+  sd zero, 8(a0)
+  sd zero, 16(a0)
+  ld s0, 0(sp)
+  ld ra, 8(sp)
   addi sp, sp, 16
-  ret 
+  ret
 
 insert:
-    addi sp, sp, -16
-    sw ra, 12(sp)
-    sw s0, 8(sp)
-    sw s1, 4(sp)
-
-    addi s0, a0, 0
-    addi s1, a1, 0
-
-    jal ra, make_node
-
-    addi t0, s0, 0
-
+  addi sp, sp, -32
+  sd ra, 24(sp)
+  sd s0, 16(sp)
+  sd s1, 8(sp)
+  addi s0, a0, 0
+  addi s1, a1, 0
+  jal ra, make_node
+  addi t0, s0, 0
 loop:
-    lw t1, 0(t0)
-
-    blt s1, t1, go_left
-    bgt s1, t1, go_right
-
+  lw t1, 0(t0)
+  blt s1, t1, go_left
+  bgt s1, t1, go_right
 go_right:
-    lw t2, 8(t0)
-    beq t2, zero, insert_right
-    addi t0, t2, 0
-    j loop
-
+  ld t2, 16(t0)
+  beq t2, zero, insert_right
+  addi t0, t2, 0
+  j loop
 insert_right:
-    sw a0, 8(t0)
-    j done
-
+  sd a0, 16(t0)
+  j done
 go_left:
-    lw t2, 4(t0)
-    beq t2, zero, insert_left
-    addi t0, t2, 0
-    j loop
-
+  ld t2, 8(t0)
+  beq t2, zero, insert_left
+  addi t0, t2, 0
+  j loop
 insert_left:
-    sw a0, 4(t0)
-
+  sd a0, 8(t0)
 done:
-    addi a0, s0, 0 #returns the root
-    lw ra, 12(sp)
-    lw s0, 8(sp)
-    lw s1, 4(sp)
-    addi sp, sp, 16
-    ret
+  addi a0, s0, 0
+  ld ra, 24(sp)
+  ld s0, 16(sp)
+  ld s1, 8(sp)
+  addi sp, sp, 32
+  ret
 
 get:
   beq a0, zero, fail
   lw t0, 0(a0)
   beq t0, a1, success
-
   blt a1, t0, move_left
   bgt a1, t0, move_right
-
-  move_left:
-    lw a0, 4(a0)
-    j get
-  move_right:
-    lw a0, 8(a0)
-    j get
-  fail:
-    addi a0, zero, 0
-    ret
-  success:
-    ret
+move_left:
+  ld a0, 8(a0)
+  j get
+move_right:
+  ld a0, 16(a0)
+  j get
+fail:
+  addi a0, zero, 0
+  ret
+success:
+  ret
 
 getAtMost:
   li   t0, -1
   addi t1, a1, 0
-
 loop:
   beq  t1, zero, finish
   lw   t2, 0(t1)
   beq  t2, a0, match
   bgt  t2, a0, left
-
   addi t0, t2, 0
-  lw   t1, 8(t1)
+  ld   t1, 16(t1)
   j    loop
-
 left:
-  lw   t1, 4(t1)
+  ld   t1, 8(t1)
   j    loop
-
 match:
   addi t0, t2, 0
   j    finish
-
 finish:
   addi a0, t0, 0
   ret
-
-
-  
